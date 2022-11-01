369f37f90c35b4ed8c8c21dce83d575c32cf496d
jcubit <jav.cuesta@gmail.com>
2022-11-01 10:50:54 +0100

    added Vector3

---
+++ src/Point.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Javier Cuesta on 30.10.22.
+//
+
+#include "Point.h"
---
+++ src/Point.h
@@ -0,0 +1,13 @@
+#ifndef PGEOCL_POINT_H
+#define PGEOCL_POINT_H
+
+template <typename T>
+struct Point3 {
+
+
+private:
+    Vector3<T> coordinates{};
+};
+
+
+#endif //PGEOCL_POINT_H
