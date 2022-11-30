141ac630aade2991415e5538c45831bac6f80c30
jcubit <jav.cuesta@gmail.com>
2022-11-30 08:21:26 +0100

    added join test

--- tests/test_join_and_meet.cpp
+++ tests/test_join_and_meet.cpp
@@ -12,9 +12,22 @@ TEST(Point, Meet) {
 
     // mut
     pgeo::Point3f p = pgeo::meet(a1,a2,a3);
-    //print(p, "p");
-
 
     EXPECT_EQ(p, pgeo::Point3f({0,1,0,1}));
+}
 
+TEST(Point, Join) {
+
+    pgeo::Point3f p1 = {0.5f,-0.5f,-0.5f,1.f};
+    pgeo::Point3f p3 = {0.5f, 0.5f,-0.5f,1.f};
+    pgeo::Point3f p5 = {0.5f,-0.5f, 0.5f,1.f};
+    pgeo::Point3f p7 = {0.5f, 0.5f, 0.5f,1.f};
+
+    // mut
+    pgeo::Plane3f a = pgeo::join(p1,p3,p5);
+    pgeo::Plane3f b = pgeo::join(p7,p3,p5);
+    print(a, "a");
+    print(b, "b");
+
+    EXPECT_EQ(a, b);
 }
